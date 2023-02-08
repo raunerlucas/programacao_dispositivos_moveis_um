471d6da5ff5c7a246d8f552afebd33ecd6c61499
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-02-08 16:17:11 -0300

    Create CActivity.java

---
+++ aula_cinco/codigos/CActivity.java
@@ -0,0 +1,11 @@
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cactivity);
+    }
+}
