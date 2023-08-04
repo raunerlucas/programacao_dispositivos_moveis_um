fe631af2bc8c6c6c41ef9e702916f5958933fadd
apag <99103993+apag-guimaraes@users.noreply.github.com>
2023-08-04 20:48:11 -0300

    Update ActivityBolo.java

--- aula_tres/codigos/ActivityBolo.java
+++ aula_tres/codigos/ActivityBolo.java
@@ -1,4 +1,4 @@
-package com.example.applanchonete;
+package com.example.app_lanchonete;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,11 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.google.android.material.textfield.TextInputEditText;
+import android.widget.EditText;
 
 public class ActivityBolo extends AppCompatActivity {
-    private TextInputEditText textInputUnidadeBolo;
+    private EditText editTextQuantidadeBolo;
     private Button buttonAdicionar;
     private Double valores;
 
@@ -19,7 +18,7 @@ public class ActivityBolo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bolo);
 
-        textInputUnidadeBolo = findViewById(R.id.textInputUnidadeBolo);
+        editTextQuantidadeBolo = findViewById(R.id.editTextQuantidadeBolo);
         buttonAdicionar = findViewById(R.id.buttonAdicionar);
         Intent intent = getIntent();
         if (intent != null){
@@ -28,11 +27,11 @@ public class ActivityBolo extends AppCompatActivity {
     }
 
     public void clicar(View view){
-        int unidade = Integer.parseInt(textInputUnidadeBolo.getText().toString());
+        int unidade = Integer.parseInt(editTextQuantidadeBolo.getText().toString());
         double total = (unidade * 4.50);
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         valores = valores + total;
-        intent.putExtra("totalBolo", valores);
+        intent.putExtra("total", valores);
         startActivity(intent);
     }
 }
