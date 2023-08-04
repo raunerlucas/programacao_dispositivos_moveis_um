5dcdf2c6f28e66714d1408ca31213db9ee919d27
apag <99103993+apag-guimaraes@users.noreply.github.com>
2023-08-04 20:46:54 -0300

    Update MainActivity.java

--- aula_tres/codigos/MainActivity.java
+++ aula_tres/codigos/MainActivity.java
@@ -1,6 +1,4 @@
-package com.example.applanchonete;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.app_lanchonete;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,50 +7,55 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity{
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
     private ImageView imageViewBolo, imageViewCafe;
     private Button buttonCalcular;
     private TextView textViewResultado;
-    private Double valores;
+    private double valores;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageViewBolo = findViewById(R.id.imageViewBolo);
-        imageViewCafe = findViewById(R.id.imageViewCafe);
-        buttonCalcular = findViewById(R.id.buttonCalcular);
+        imageViewBolo = findViewById(R.id.imageBolo);
+        imageViewCafe = findViewById(R.id.imageCafe);
+        buttonCalcular = findViewById(R.id.btCalcular);
         textViewResultado = findViewById(R.id.textViewResultado);
 
         Intent intent = getIntent();
-        if (intent != null){
+        if (intent != null) {
             double valor = intent.getDoubleExtra("total", 0);
-            double valorBolo = intent.getDoubleExtra("totalBolo", 0);
-            valores = valor + valorBolo;
+            // double valorBolo = intent.getDoubleExtra("totalBolo", 0);
+            valores = valores + valor;
+        }
+
+
+    }
+
+    public void escolher(View view) {
+        if (R.id.imageBolo == view.getId()) {
+            Intent intentBolo = new Intent(getApplicationContext(), ActivityBolo.class);
+            if (valores != 0) {
+                intentBolo.putExtra("valores", valores);
+            }
+            startActivity(intentBolo);
+        }
+
+        if (R.id.imageCafe == view.getId()) {
+            Intent intentCafe = new Intent(getApplicationContext(), ActivityCafe.class);
+            if (valores != 0) {
+                intentCafe.putExtra("valores", valores);
+            }
+            startActivity(intentCafe);
+        }
+
+        if (R.id.btCalcular == view.getId()) {
+            textViewResultado.setText(String.valueOf(valores));
         }
     }
 
-    public  void click(View view){
-        switch (view.getId()){
-            case R.id.imageViewBolo:
-                Intent intentBolo = new Intent(getApplicationContext(), ActivityBolo.class);
-                if(valores != 0){
-                    intentBolo.putExtra("valores", valores);
-                }
-                startActivity(intentBolo);
-                break;
-            case R.id.imageViewCafe:
-                Intent intentCafe = new Intent(getApplicationContext(), ActivityCafe.class);
-                if(valores != 0){
-                    intentCafe.putExtra("valores", valores);
-                }
-                startActivity(intentCafe);
-                break;
-            case R.id.buttonCalcular:
-                textViewResultado.setText(valores.toString());
-                break;
-
-        }
-    }
-}
+}//class
